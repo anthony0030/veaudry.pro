4c76cb99fd20939b0e9142f3ee7a83a64f4dc29f
Anthony Veaudry <anthony@veaudry.pro>
2017-07-21 23:11:42 +0300

    update face changer
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,14 @@
 
-	var on=0;
-function switch_face() {
+	
+var on=0;
 
+function switch_face() {
 	if (on==0) {
-		on=1;
 		document.getElementById('my_face').src ="http://www.gravatar.com/avatar/5b116beaa14acdb21cf5f33ecdd32be2?s=150";
+		on=1;
 	} 
 	else {
-		on=0;
 		document.getElementById('my_face').src ="IMG/anthony/face.jpg";
+		on=0;
 	}
 }
