667ab7f726a9a0e4b42b67d4d6c5df027cadb938
Anthony Veaudry <anthony@veaudry.pro>
2017-08-28 17:40:09 +0300

    make gravatar profile photo secure
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- JS/script.js
+++ JS/script.js
@@ -4,7 +4,7 @@ var on=0;
 
 function switch_face() {
 	if (on==0) {
-		document.getElementById('my_face').src ="http://www.gravatar.com/avatar/5b116beaa14acdb21cf5f33ecdd32be2?s=150";
+		document.getElementById('my_face').src ="https://www.gravatar.com/avatar/5b116beaa14acdb21cf5f33ecdd32be2?s=150";
 		on=1;
 	} 
 	else {
