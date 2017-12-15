c21b4efaf02a0a3a174deb696e788d88bc723cd4
Anthony Veaudry <anthony@veaudry.pro>
2017-12-15 19:08:22 +0200

    Revert "refactor code"
    This reverts commit 7850e85be810e2054c33a0c4f0679f8f3892600a.
    
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>
    
    # Conflicts:
    #	JS/script.js

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,13 @@
 
-	
-var on = true;
+var on = 0;
 
 function switchFace() {
-	if (on) {
-		document.getElementById("my_face").src ="https://www.gravatar.com/avatar/5b116beaa14acdb21cf5f33ecdd32be2?s=150";
-		on = true;
-	} 
-	else {
-		document.getElementById("my_face").src ="IMG/anthony/face.jpg";
-		on = false;
-	}
+  if (on === 0) {
+    document.getElementById('my_face').src ="https://www.gravatar.com/avatar/5b116beaa14acdb21cf5f33ecdd32be2?s=150";
+    on = 1;
+  } 
+  else {
+    document.getElementById('my_face').src ="IMG/anthony/face.jpg";
+    on = 0;
+  }
 }
