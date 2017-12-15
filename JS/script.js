84e16ef4415823cfdb7f4b7eccba64c76c557dc4
Anthony Veaudry <anthony@veaudry.pro>
2017-12-15 19:00:12 +0200

    camelcase function
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@
 	
 var on = true;
 
-function switch_face() {
+function switchFace() {
 	if (on) {
 		document.getElementById("my_face").src ="https://www.gravatar.com/avatar/5b116beaa14acdb21cf5f33ecdd32be2?s=150";
 		on = true;
