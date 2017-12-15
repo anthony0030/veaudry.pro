7850e85be810e2054c33a0c4f0679f8f3892600a
Anthony Veaudry <anthony@veaudry.pro>
2017-12-15 18:57:36 +0200

    refactor code
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,14 @@
 
 	
-var on=0;
+var on = true;
 
 function switch_face() {
-	if (on==0) {
-		document.getElementById('my_face').src ="https://www.gravatar.com/avatar/5b116beaa14acdb21cf5f33ecdd32be2?s=150";
-		on=1;
+	if (on) {
+		document.getElementById("my_face").src ="https://www.gravatar.com/avatar/5b116beaa14acdb21cf5f33ecdd32be2?s=150";
+		on = true;
 	} 
 	else {
-		document.getElementById('my_face').src ="IMG/anthony/face.jpg";
-		on=0;
+		document.getElementById("my_face").src ="IMG/anthony/face.jpg";
+		on = false;
 	}
 }
