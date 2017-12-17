f044b0c41ef71f368ef27b4f1196b8ecca9f57c2
Anthony Veaudry <anthony@veaudry.pro>
2017-12-17 12:38:12 +0200

    change quotes for double quotes in the script file
    Signed-off-by: Anthony Veaudry <anthony@veaudry.pro>

--- JS/script.js
+++ JS/script.js
@@ -3,11 +3,11 @@ var on = 0;
 
 function switchFace() {
   if (on === 0) {
-    document.getElementById('my_face').src ="https://www.gravatar.com/avatar/5b116beaa14acdb21cf5f33ecdd32be2?s=150";
+    document.getElementById("my_face").src ="https://www.gravatar.com/avatar/5b116beaa14acdb21cf5f33ecdd32be2?s=150";
     on = 1;
   } 
   else {
-    document.getElementById('my_face').src ="IMG/anthony/face.jpg";
+    document.getElementById("my_face").src ="IMG/anthony/face.jpg";
     on = 0;
   }
 }
